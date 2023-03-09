655b6ce79b8cc34bce9b06ec2d14e611d4d329c7
Bhut Tushar <bhuttushar2019@gmail.com>
2023-03-09 22:37:58 +0530

    iButton, random states : Done inversion state validation: Pending

--- app/src/main/java/com/example/a8puzzlesolver/MainActivity.kt
+++ app/src/main/java/com/example/a8puzzlesolver/MainActivity.kt
@@ -1,40 +1,103 @@
 package com.example.a8puzzlesolver
 
-import android.animation.ObjectAnimator
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
-import java.util.*
-import java.util.Collections.swap
-import kotlin.math.log
+import android.view.*
+import android.view.animation.AlphaAnimation
+import android.view.animation.AnimationUtils
+import android.widget.*
+import com.example.a8puzzlesolver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var buttons: List<List<Button>>
+    private lateinit var binding: ActivityMainBinding
     var emptyButtonIndex_i: Int = 2
     var emptyButtonIndex_j: Int = 2
+    private lateinit var popupWindow: PopupWindow
+    private lateinit var handler: Handler
+    private lateinit var runnable: Runnable
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        textView = findViewById(R.id.i_goal_state_text)
+
+        val handler = Handler()
+        val runnable = object : Runnable {
+            override fun run() {
+                // Show the textview with fade-in animation
+                textView.visibility = View.VISIBLE
+                textView.startAnimation(AnimationUtils.loadAnimation(this@MainActivity, R.anim.i_button_fade_in))
+
+                Handler().postDelayed({
+                    textView.visibility = View.GONE
+                }, 5000)
+
+                // Repeat the animation after 15 seconds
+                handler.postDelayed(this, 15000)
+            }
+        }
+        handler.post(runnable)
 
 
         buttons = listOf(
-            listOf(findViewById(R.id.button1),
-            findViewById(R.id.button2),
-            findViewById(R.id.button3)),
-            listOf(findViewById(R.id.button4),
-            findViewById(R.id.button5),
-            findViewById(R.id.button6)),
-            listOf(findViewById(R.id.button7),
-            findViewById(R.id.button8),
-            findViewById(R.id.button9))
+            listOf(
+                findViewById(R.id.button1),
+                findViewById(R.id.button2),
+                findViewById(R.id.button3)
+            ),
+            listOf(
+                findViewById(R.id.button4),
+                findViewById(R.id.button5),
+                findViewById(R.id.button6)
+            ),
+            listOf(
+                findViewById(R.id.button7),
+                findViewById(R.id.button8),
+                findViewById(R.id.button9)
+            )
         )
 
+        val initState = intent.getStringArrayListExtra("initState")
+        val goalState = intent.getStringArrayListExtra("goalState")
+//
+        var index = 0
+        for (i in 0..2) {
+            for (j in 0..2) {
+//                Log.e("Value at $i,$j", "${initState?.get(index)}", )
+                buttons[i][j].text = initState!![index].toString()
+                if (buttons[i][j].text.toString() == "0") {
+                    emptyButtonIndex_i = i
+                    emptyButtonIndex_j = j
+                }
+                index++
+            }
+        }
+        val popupView = LayoutInflater.from(this).inflate(R.layout.activity_goal_state_popup, null)
+        popupWindow = PopupWindow(popupView, 800, 800)
+        popupWindow.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        popupWindow.animationStyle = R.style.PopupAnimation
+
+        val closeButton = popupView.findViewById<Button>(R.id.close_button)
+        closeButton.setOnClickListener {
+            popupWindow.dismiss()
+        }
+
+        popupView.findViewById<Button>(R.id.close_button).setOnClickListener {
+            popupWindow.dismiss()
+        }
+        binding.iButton.setOnClickListener{
+            popupWindow.showAtLocation(binding.iButton, Gravity.CENTER, 0, 0)
+        }
+
+        buttons[emptyButtonIndex_i][emptyButtonIndex_j].setBackgroundColor(Color.parseColor("#5a5a5a"))
         buttons.forEachIndexed { i, row ->
             row.forEachIndexed { j, button ->
                 button.setOnClickListener {
@@ -45,83 +108,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-//    private fun swapButtons(i: Int, j: Int) {
-//        val buttonToMove = buttons[i][j]
-//        val emptyButton = buttons[emptyButtonIndex_i][emptyButtonIndex_j]
-//
-//        // Check if the clicked button is adjacent to the empty button (not diagonally)
-//        var isAdjacent = false
-//        if (i - 1 >= 0 && buttons[i - 1][j].text.toString() == "") {
-//            isAdjacent = true
-//            emptyButtonIndex_i = i - 1
-//            emptyButtonIndex_j = j
-//        } else if (i + 1 < 3 && buttons[i + 1][j].text.toString() == "") {
-//            isAdjacent = true
-//            emptyButtonIndex_i = i + 1
-//            emptyButtonIndex_j = j
-//        } else if (j - 1 >= 0 && buttons[i][j - 1].text.toString() == "") {
-//            isAdjacent = true
-//            emptyButtonIndex_i = i
-//            emptyButtonIndex_j = j - 1
-//        } else if (j + 1 < 3 && buttons[i][j + 1].text.toString() == "") {
-//            isAdjacent = true
-//            emptyButtonIndex_i = i
-//            emptyButtonIndex_j = j + 1
-//        }
-//
-//        if (isAdjacent) {
-//            // Animate the movement of the buttons using ObjectAnimator
-//            val xDiff = emptyButton.x - buttonToMove.x
-//            val yDiff = emptyButton.y - buttonToMove.y
-//
-//            ObjectAnimator.ofFloat(buttonToMove, View.TRANSLATION_X, xDiff)
-//                .apply { duration = 200 }
-//                .start()
-//            ObjectAnimator.ofFloat(buttonToMove, View.TRANSLATION_Y, yDiff)
-//                .apply { duration = 200 }
-//                .start()
-//            ObjectAnimator.ofFloat(emptyButton, View.TRANSLATION_X, -xDiff)
-//                .apply { duration = 200 }
-//                .start()
-//            ObjectAnimator.ofFloat(emptyButton, View.TRANSLATION_Y, -yDiff)
-//                .apply { duration = 200 }
-//                .start()
-//
-//            // Swap the buttons in the 2D list
-////            val temp = buttons[i][j]
-////            buttons[i][j] = buttons[emptyButtonIndex_i][emptyButtonIndex_j]
-////            buttons[emptyButtonIndex_i][emptyButtonIndex_j] = temp
-//            buttons[emptyButtonIndex_i][emptyButtonIndex_j].text = buttons[i][j].text
-//            buttons[i][j].text = ""
-//            for(i in buttons.indices){
-//                for(j in buttons[i].indices){
-//                    Log.e("Button $i,$j", "${buttons[i][j].text}")
-//                }
-//            }
-//        }
-//    }
-private fun swapButtons(i: Int, j: Int) {
-    val clickedButton = buttons[i][j]
-    val emptyButton = buttons[emptyButtonIndex_i][emptyButtonIndex_j]
+    private fun swapButtons(i: Int, j: Int) {
+        val clickedButton = buttons[i][j]
+        val emptyButton = buttons[emptyButtonIndex_i][emptyButtonIndex_j]
 
-    // Check if the clicked button is adjacent to the empty button (not diagonally)
-    if ((i == emptyButtonIndex_i && j == emptyButtonIndex_j - 1) // Left
-        || (i == emptyButtonIndex_i && j == emptyButtonIndex_j + 1) // Right
-        || (j == emptyButtonIndex_j && i == emptyButtonIndex_i - 1) // Up
-        || (j == emptyButtonIndex_j && i == emptyButtonIndex_i + 1) // Down
-    ) {
-        // Swap the text of the clicked button and the empty button
-        val temp = clickedButton.text.toString()
-        clickedButton.text = emptyButton.text.toString()
-        emptyButton.text = temp
+        // Check if the clicked button is adjacent to the empty button (not diagonally)
+        if ((i == emptyButtonIndex_i && j == emptyButtonIndex_j - 1) // Left
+            || (i == emptyButtonIndex_i && j == emptyButtonIndex_j + 1) // Right
+            || (j == emptyButtonIndex_j && i == emptyButtonIndex_i - 1) // Up
+            || (j == emptyButtonIndex_j && i == emptyButtonIndex_i + 1) // Down
+        ) {
+            // Swap the text of the clicked button and the empty button
+            val temp = clickedButton.text.toString()
+            clickedButton.text = emptyButton.text.toString()
+            emptyButton.text = temp
 
-        // Update the indices of the empty button
-        emptyButtonIndex_i = i
-        emptyButtonIndex_j = j
+            // Update the indices of the empty button
+            emptyButtonIndex_i = i
+            emptyButtonIndex_j = j
 
-        // Update the background color of the buttons to indicate the swap
-        clickedButton.setBackgroundColor(Color.parseColor("#5a5a5a"))
-        emptyButton.setBackgroundColor(Color.parseColor("#f0f0f0"))
+            // Update the background color of the buttons to indicate the swap
+            clickedButton.setBackgroundColor(Color.parseColor("#5a5a5a"))
+            emptyButton.setBackgroundColor(Color.parseColor("#f0f0f0"))
+        }
     }
-}
 }
--- app/src/main/java/com/example/a8puzzlesolver/stateInitActivity.kt
+++ app/src/main/java/com/example/a8puzzlesolver/stateInitActivity.kt
@@ -1,153 +1,172 @@
 package com.example.a8puzzlesolver
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.text.set
 import com.example.a8puzzlesolver.databinding.ActivityStateInitBinding
 
 class stateInitActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStateInitBinding
-    private lateinit var initialStateArray: ArrayList<Int>
-    private lateinit var goalStateArray: ArrayList<Int>
+    private lateinit var initialStateArray: ArrayList<String>
+    private lateinit var goalStateArray: ArrayList<String>
+//    private lateinit var randomStatesArray: ArrayList<ArrayList<Int>>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStateInitBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initialStateArray = ArrayList()
+        goalStateArray = ArrayList()
 
-        binding.buttonSolve.setOnClickListener{
-            if(validateTable()){
-//                Toast.makeText(this, "Valid", Toast.LENGTH_SHORT).show()
-            }
-            else
-            {
-//                Toast.makeText(this, "Not Valid", Toast.LENGTH_SHORT).show()
+        val randomStatesArray = listOf(
+            listOf(2, 8, 3, 1, 6, 4, 7, 5, 0),
+            listOf(4, 6, 1, 5, 8, 2, 7, 3, 0),
+            listOf(1, 4, 2, 3, 0, 5, 6, 7, 8),
+            listOf(7, 2, 4, 1, 5, 0, 3, 8, 6),
+            listOf(8, 2, 3, 7, 1, 6, 0, 5, 4)
+        )
+
+        binding.buttonRandom.setOnClickListener {
+            val randomState = randomStatesArray.random()
+//            Toast.makeText(this, randomState[0].toString(), Toast.LENGTH_SHORT).show()
+            binding.init00.setText(randomState[0].toString())
+            binding.init01.setText(randomState[1].toString())
+            binding.init02.setText(randomState[2].toString())
+            binding.init10.setText(randomState[3].toString())
+            binding.init11.setText(randomState[4].toString())
+            binding.init12.setText(randomState[5].toString())
+            binding.init20.setText(randomState[6].toString())
+            binding.init21.setText(randomState[7].toString())
+            binding.init22.setText(randomState[8].toString())
+
+            binding.goal00.setText("1")
+            binding.goal01.setText("2")
+            binding.goal02.setText("3")
+            binding.goal10.setText("4")
+            binding.goal11.setText("5")
+            binding.goal12.setText("6")
+            binding.goal20.setText("7")
+            binding.goal21.setText("8")
+            binding.goal22.setText("0")
+        }
+
+        binding.buttonSolve.setOnClickListener {
+            if (!checkIfTilesAreEmpty()) {
                 val builder = AlertDialog.Builder(this)
-                builder.setTitle("Invalid state")
-                builder.setMessage("Invalid State!! Please Check it out.")
+                builder.setTitle("Tiles are empty")
+                builder.setMessage("Some of tiles are empty!! Please Check it out.")
+                builder.setPositiveButton("OK") { dialog, which ->
+                    // Do nothing, just close the dialog
+                }
+                builder.show()
+            } else if (hasDuplicate()) {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Contains Duplicate")
+                builder.setMessage("Some of tiles are duplicate!! Please Check it out.")
                 builder.setPositiveButton("OK") { dialog, which ->
                     // Do nothing, just close the dialog
                 }
                 builder.show()
             }
+//            if(solvable()){
+//
+//            }
+            else {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putStringArrayListExtra("initState", initialStateArray)
+                intent.putStringArrayListExtra("goalState", goalStateArray)
+
+                startActivity(intent)
+            }
         }
     }
 
-    private fun validateTable():Boolean{
+    private fun checkIfTilesAreEmpty(): Boolean {
 
-        if(binding.init00.text.toString()!="")
-        {
-            initialStateArray.add(binding.init00.text.toString().toInt())
-        }
-        else return false
+        if (binding.init00.text.toString() != "") {
+            initialStateArray.add(binding.init00.text.toString())
+        } else return false
 
-        if(binding.init01.text.toString()!="")
-        {
-            initialStateArray.add(binding.init01.text.toString().toInt())
-        }
-        else return false
+        if (binding.init01.text.toString() != "") {
+            initialStateArray.add(binding.init01.text.toString())
+        } else return false
 
-        if(binding.init02.text.toString()!="")
-        {
-            initialStateArray.add(binding.init02.text.toString().toInt())
-        }
-        else return false
+        if (binding.init02.text.toString() != "") {
+            initialStateArray.add(binding.init02.text.toString())
+        } else return false
 
-        if(binding.init10.text.toString()!="")
-        {
-            initialStateArray.add(binding.init10.text.toString().toInt())
-        }
-        else return false
+        if (binding.init10.text.toString() != "") {
+            initialStateArray.add(binding.init10.text.toString())
+        } else return false
 
-        if(binding.init11.text.toString()!="")
-        {
-            initialStateArray.add(binding.init11.text.toString().toInt())
-        }
-        else return false
+        if (binding.init11.text.toString() != "") {
+            initialStateArray.add(binding.init11.text.toString())
+        } else return false
 
-        if(binding.init12.text.toString()!="")
-        {
-            initialStateArray.add(binding.init12.text.toString().toInt())
-        }
-        else return false
+        if (binding.init12.text.toString() != "") {
+            initialStateArray.add(binding.init12.text.toString())
+        } else return false
 
-        if(binding.init20.text.toString()!="")
-        {
-            initialStateArray.add(binding.init20.text.toString().toInt())
-        }
-        else return false
+        if (binding.init20.text.toString() != "") {
+            initialStateArray.add(binding.init20.text.toString())
+        } else return false
 
-        if(binding.init21.text.toString()!="")
-        {
-            initialStateArray.add(binding.init21.text.toString().toInt())
-        }
-        else return false
+        if (binding.init21.text.toString() != "") {
+            initialStateArray.add(binding.init21.text.toString())
+        } else return false
 
-        if(binding.init22.text.toString()!="")
-        {
-            initialStateArray.add(binding.init22.text.toString().toInt())
-        }
-        else return false
+        if (binding.init22.text.toString() != "") {
+            initialStateArray.add(binding.init22.text.toString())
+        } else return false
 
         //goal
-        if(binding.goal00.text.toString()!="")
-        {
-            goalStateArray.add(binding.init00.text.toString().toInt())
-        }
-        else return false
+        if (binding.goal00.text.toString() != "") {
+            goalStateArray.add(binding.init00.text.toString())
+        } else return false
 
-        if(binding.goal01.text.toString()!="")
-        {
-            goalStateArray.add(binding.init01.text.toString().toInt())
-        }
-        else return false
+        if (binding.goal01.text.toString() != "") {
+            goalStateArray.add(binding.init01.text.toString())
+        } else return false
 
-        if(binding.goal02.text.toString()!="")
-        {
-            goalStateArray.add(binding.init02.text.toString().toInt())
-        }
-        else return false
+        if (binding.goal02.text.toString() != "") {
+            goalStateArray.add(binding.init02.text.toString())
+        } else return false
 
-        if(binding.goal10.text.toString()!="")
-        {
-            goalStateArray.add(binding.init10.text.toString().toInt())
-        }
-        else return false
+        if (binding.goal10.text.toString() != "") {
+            goalStateArray.add(binding.init10.text.toString())
+        } else return false
 
-        if(binding.goal11.text.toString()!="")
-        {
-            goalStateArray.add(binding.init11.text.toString().toInt())
-        }
-        else return false
+        if (binding.goal11.text.toString() != "") {
+            goalStateArray.add(binding.init11.text.toString())
+        } else return false
 
-        if(binding.goal12.text.toString()!="")
-        {
-            goalStateArray.add(binding.init12.text.toString().toInt())
-        }
-        else return false
+        if (binding.goal12.text.toString() != "") {
+            goalStateArray.add(binding.init12.text.toString())
+        } else return false
 
-        if(binding.goal20.text.toString()!="")
-        {
-            goalStateArray.add(binding.init20.text.toString().toInt())
-        }
-        else return false
+        if (binding.goal20.text.toString() != "") {
+            goalStateArray.add(binding.init20.text.toString())
+        } else return false
 
-        if(binding.goal21.text.toString()!="")
-        {
-            goalStateArray.add(binding.init21.text.toString().toInt())
-        }
-        else return false
+        if (binding.goal21.text.toString() != "") {
+            goalStateArray.add(binding.init21.text.toString())
+        } else return false
 
-        if(binding.goal22.text.toString()!="")
-        {
-            goalStateArray.add(binding.init22.text.toString().toInt())
-        }
-        else return false
-
-        if((initialStateArray.size != initialStateArray.distinct().size) or (goalStateArray.size != goalStateArray.distinct().size))return false
-
-        //todo : validate the state such that it won't go in infinite loop.
+        if (binding.goal22.text.toString() != "") {
+            goalStateArray.add(binding.init22.text.toString())
+        } else return false
         return true
     }
 
+    private fun hasDuplicate(): Boolean {
+        if ((initialStateArray.size != initialStateArray.distinct().size) or (goalStateArray.size != goalStateArray.distinct().size)) return true
+        return false
+    }
+
+//    private fun solvable():Boolean{
+//        //todo : validate the state such that it won't go in infinite loop.
+//    }
 }
